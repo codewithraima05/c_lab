663c30533952e36f2e47c403ff943596f2cda2af
Raima Maurya <mauryaraima5@gmail.com>
2026-08-19 21:28:10 +0530

    day 10b program

---
+++ weeks.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("ENTER THE DAY OF THE WEEK");
+    scanf("%d",&n);
+    switch(n)
+    {
+        case 1:printf("MONDAY");break;
+        case 2:printf("TUESDAY");break;
+        case 3:printf("WEDNESDAY");break;
+        case 4:printf("THURSDAY");break;
+        case 5:printf("FRIDAY");break;
+        case 6:printf("SATURDAY");break;
+        case 7:printf("SUNDAY");break;
+        default:printf("INVALID INPUT");
+    }
+    return 0;
+}
