aa2bbd7288c34a82aadb2facfa302622b616c39f
Raima Maurya <mauryaraima5@gmail.com>
2026-08-22 22:07:36 +0530

    add 13a program

---
+++ .vscode/natural.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("ENTER THE RANGE");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        printf("%d " , i);
+    }
+    return 0;
+}
